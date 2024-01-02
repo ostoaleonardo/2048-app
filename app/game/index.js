700dcdfc65e12d292d65830a1077b04880cad1f6
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-01-02 17:20:54 -0600

    Add swipe gesture detector

--- app/game/index.js
+++ app/game/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
+import { runOnJS } from 'react-native-reanimated'
 import { Board } from '../../src/components/Board'
 import { Tile } from '../../src/components/Tile'
 
 export default function Game() {
     const [board, setBoard] = useState()
+    const [score, setScore] = useState(0)
     const ROWS = 4
 
     useEffect(() => {
@@ -43,15 +46,84 @@ export default function Game() {
         return newBoard
     }
 
+    const moveLeft = () => {
+        console.log('moveLeft')
+
+        const newBoard = board.map((row) => {
+            const newRow = []
+            let score = 0
+
+            // Filter and combine numbers
+            for (let i = 0; i < row.length; i++) {
+                if (row[i] !== 0) {
+                    if (newRow.length > 0 && newRow[newRow.length - 1] === row[i]) {
+                        // Combine equal numbers
+                        newRow[newRow.length - 1] *= 2
+                        // Add to score
+                        score += newRow[newRow.length - 1]
+                    } else {
+                        newRow.push(row[i])
+                    }
+                }
+            }
+
+            // Fill the rest of the row with zeros
+            while (newRow.length < ROWS) {
+                newRow.push(0)
+            }
+
+            return newRow
+        })
+
+        // Check if the board has changed
+        const isBoardChanged = JSON.stringify(board) !== JSON.stringify(newBoard)
+
+        // Add a random number if the board has changed
+        if (isBoardChanged) {
+            const updatedBoard = addRandomNumber(newBoard)
+            setBoard(updatedBoard)
+            setScore(score)
+        }
+    }
+
+    const moveRight = () => {
+        console.log('moveRight')
+    }
+
+    const moveUp = () => {
+        console.log('moveUp')
+    }
+
+    const moveDown = () => {
+        console.log('moveDown')
+    }
+
+    const panGesture = Gesture.Pan()
+        .onEnd(({ translationX, translationY }) => {
+            if (translationX > 0 && translationX > translationY) {
+                runOnJS(moveRight)()
+            } else if (translationX < 0 && translationX < translationY) {
+                runOnJS(moveLeft)()
+            } else if (translationY > 0 && translationY > translationX) {
+                runOnJS(moveDown)()
+            } else {
+                runOnJS(moveUp)()
+            }
+        })
+
+
     return (
         <View style={styles.container}>
-            <Board rows={ROWS}>
-                {board && board.map((row, rowIndex) => {
-                    return row.map((value, columnIndex) => {
-                        return <Tile key={rowIndex + columnIndex} value={value} />
-                    })
-                })}
-            </Board>
+            <Text style={styles.score}>Score: {score}</Text>
+            <GestureDetector gesture={panGesture}>
+                <Board rows={ROWS}>
+                    {board && board.map((row, rowIndex) => {
+                        return row.map((value, columnIndex) => {
+                            return <Tile key={rowIndex + columnIndex} value={value} />
+                        })
+                    })}
+                </Board>
+            </GestureDetector>
         </View>
     )
 }
@@ -64,4 +136,9 @@ const styles = StyleSheet.create({
         paddingHorizontal: 30,
         backgroundColor: 'white',
     },
+    score: {
+        fontSize: 30,
+        marginBottom: 20,
+        fontWeight: 'bold',
+    }
 })
